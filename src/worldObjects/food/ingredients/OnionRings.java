214c07a987059d538e40f9d1aeb3134ed39de473
xgron <niclas_folster@hotmail.se>
2013-09-04 09:55:27 +0200

    Ändrade så att Onionrings är rättstavat
    nub

--- src/worldObjects/food/ingredients/OnionRings.java
+++ src/worldObjects/food/ingredients/OnionRings.java
@@ -1,10 +1,10 @@
 package worldObjects.food.ingredients;
 
-public class UnionRings extends GIngredient implements Activateable{
+public class OnionRings extends GIngredient implements Activateable{
 
-	public UnionRings(float x, float y, float size) {
+	public OnionRings(float x, float y, float size) {
 		super(x, y, 100, 8, 0.45f, 0.55f);
-		setTexture("unionRings");
+		setTexture("onionRings");
 	}
 
 	public void useFirstAbility() {
