296821e238698245f3741a5f4a500ffdb55360ad
xgron <niclas_folster@hotmail.se>
2013-11-28 08:16:48 +0100

    k

--- src/game/parts/Actionbar.java
+++ src/game/parts/Actionbar.java
@@ -13,10 +13,8 @@ import game.GamePart;
 
 public class Actionbar implements GamePart{
 	
-	HealthBar healthBar = new HealthBar(170, Display.getHeight()-75, 300, 20, 0.2f, 0.8f, 0.2f, 1f, GameWorld.getPlayer());
-	GSprite healthBarBackground = new GSprite(healthBar.getX(), healthBar.getY() , healthBar.getTexWidth() + 6, healthBar.getTexHeight() + 6, 0.8f,0.8f,0.8f,1f);
 		
-	GSprite background = new GSprite(Display.getWidth()/2, Display.getHeight()-50 , Display.getWidth(), 100, 0.48f,0.69f,0.71f,0.9f);
+	GSprite background = new GSprite(Display.getWidth()/2, Display.getHeight()-40 , Display.getWidth()-400, 80, "UI/actionbar");
 	public void handleInput() {
 		 
 	}
@@ -30,9 +28,7 @@ public class Actionbar implements GamePart{
 		
 		{ //HEALTHBAR///
 			//GText.font.drawString(healthBar.getX()-155, healthBar.getY()-37, "Health", Color.yellow);
-			healthBarBackground.render();
-			healthBar.render();
-			GText.font.drawString(healthBar.getX()-145, healthBar.getY()-14, healthBar.getMaxHealth() + "/" + healthBar.getHealth() + "   HP", Color.black);
+		
 		}
 	}
 
