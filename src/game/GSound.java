df0ec28894c7c45d13a12144cbdc9faeaa3c23b2
xgron <niclas_folster@hotmail.se>
2013-12-26 21:48:19 +0100

    llul

--- src/game/GSound.java
+++ src/game/GSound.java
@@ -1,5 +1,13 @@
 package game;
 
-public class GSound {
-
-}
+import java.io.IOException;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.openal.AL;
+import org.newdawn.slick.openal.Audio;
+import org.newdawn.slick.openal.AudioLoader;
+import org.newdawn.slick.openal.SoundStore;
+import org.newdawn.slick.util.ResourceLoader;
