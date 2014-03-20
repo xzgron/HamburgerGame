c70183fe4482212c29082c10477658754aeea36c
xgron <niclas_folster@hotmail.se>
2014-03-20 11:33:30 +0100

    de

--- src/game/parts/StartMenu.java
+++ src/game/parts/StartMenu.java
@@ -20,7 +20,7 @@ import org.newdawn.slick.opengl.Texture;
 
 public class StartMenu extends GamePart {
 	
-	GSprite background = new GSprite(Display.getWidth()/2,Display.getHeight()/2, Display.getWidth(), Display.getHeight(), "UI/background");
+	GSprite background = new GSprite(Display.getWidth()/2,Display.getHeight()/2, Display.getWidth(), Display.getHeight(), 0.48f,0.69f,0.71f,0.9f);
 	
 	GButton startButton = new GButton(Display.getWidth()/2,Display.getHeight()/2-100, 300, 80);
 	GButton optionButton = new GButton(Display.getWidth()/2,Display.getHeight()/2, 300, 80);
